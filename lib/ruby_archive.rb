892f6fedbc60b25ea75d7a3f38234ce10e310572
Jonathan Nielsen <jonathan@jmnet.us>
2010-08-30 21:49:35 -0600

    changed everything from def self.### to use module_function

--- lib/ruby_archive.rb
+++ lib/ruby_archive.rb
@@ -14,29 +14,31 @@ module RubyArchive
   @@handlers ||= []
   # Adds an archive handler to the list used.  Provided handler must be a
   # subclass of +RubyArchive::Handler+
-  def self.add_handler_class handler_class
+  def add_handler_class handler_class
     unless (handler_class.is_a? Class) && (handler_class <= RubyArchive::Handler)
       raise TypeError, "#{handler_class} is not a RubyArchive::Handler"
     end
     @@handlers << handler_class
     true
   end
+  module_function :add_handler_class
 
   # Finds the appropriate +RubyArchive::Handler+ subclass for a given location.
   # Returns nil if no supported handler found.
-  def self.find_handler_class location
+  def find_handler_class location
     @@handlers.each do |h|
       return h if h.handles?(location)
     end
     return nil
   end
+  module_function :find_handler_class
 
   @@loaded_archives ||= {}
   # Retrieves an archive from the loaded_archives cache, or automatically
   # loads the archive.  Returns the archive on success.  Returns nil if
   # archive is not available and autoload is false.  Raises +LoadError+
   # if autoload is attempted and fails.
-  def self.get location, autoload=true
+  def get location, autoload=true
     @@handlers.each do |h|
       normalized = h.normalize_path(location)
       return @@loaded_archives[normalized] if @@loaded_archives.has_key?(normalized)
@@ -44,11 +46,12 @@ module RubyArchive
     return load(location) if autoload
     nil
   end
+  module_function :get
 
   # Loads the specified archive location.  Returns the handler object on
   # success.  Raises +LoadError+ if no handler can be found or it does not
-  # exist.  May also raise exceptions passed along by creating the handler.
-  def self.load location
+  # exist.  May also pass exceptions passed along by creating the handler.
+  def load location
     handler_class = find_handler_class(location)
     if handler_class.nil?
       raise LoadError, "No handler found or does not exist for archive -- #{location}"
@@ -56,14 +59,16 @@ module RubyArchive
     archive = handler_class.new(location)
     @@loaded_archives[archive.name] = archive
   end
+  module_function :load
 
-  def self.close_all_archives
+  def close_all_archives
     @@loaded_archives.each_value do |archive|
       archive.close
     end
     @@loaded_archives.clear
     true
   end
+  module_function :close_all_archives
 
 end
 
